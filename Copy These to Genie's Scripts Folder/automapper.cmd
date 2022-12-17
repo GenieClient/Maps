7741b6f1ddbe9cad9d9701ef388e9cb4abf40b85
HanryuDR <dahilles@gmail.com>
2022-12-17 16:28:34 -0500

    clarity on typeahead in help

--- Copy These to Genie's Scripts Folder/automapper.cmd
+++ Copy These to Genie's Scripts Folder/automapper.cmd
@@ -233,39 +233,39 @@ var autoversion 8.2022-12-17
 if def(version) then var helpecho #33CC99 mono
 else var helpecho #33CC99
 if matchre("%1", "help|HELP|Help|^$") then {
-  put #echo %helpecho <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
-  put #echo %helpecho <<  Welcome to automapper Setup!   (version %autoversion)           >>
-  put #echo %helpecho <<  Use the command line to set the following preferences:          >>
-  put #echo %helpecho <<    Typeahead                                                     >>
-  put #echo %helpecho <<      Standard Account = 1, Premium Account = 2, LTB Premium = 3  >>
-  put #echo %helpecho <<      #var automapper.typeahead 1                                 >>
-  put #echo %helpecho <<    Pause                                                         >>
-  put #echo %helpecho <<      Time to pause before sending a "put x" command              >>
-  put #echo %helpecho <<      #var automapper.pause 0.01                                  >>
-  put #echo %helpecho <<    Confirmation                                                  >>
-  put #echo %helpecho <<      1: wait for correct confirmation of sent commands           >>
-  put #echo %helpecho <<      0: don't wait                                               >>
-  put #echo %helpecho <<      #var automapper.confirmation 1                              >>
-  put #echo %helpecho <<    Infinite Loop Protection                                      >>
-  put #echo %helpecho <<      Increase if you get infinte loop errors                     >>
-  put #echo %helpecho <<      #var automapper.loop 0.001                                  >>
-  put #echo %helpecho <<    Echoes                                                        >>
-  put #echo %helpecho <<      how verbose do you want automapper to be?                   >>
-  put #echo %helpecho <<      #var automapper.verbose 1                                   >>
-  put #echo %helpecho <<    Ice Road Behavior                                             >>
-  put #echo %helpecho <<      1: collect rocks on the ice road when lacking skates        >>
-  put #echo %helpecho <<      0: just wait 15 seconds with no RT instead                  >>
-  put #echo %helpecho <<      #var automapper.iceroadcollect 1                            >>
-  put #echo %helpecho <<    Color                                                         >>
-  put #echo %helpecho <<      What should the default automapper echo color be?           >>
-  put #echo %helpecho <<      #var automapper.color #33CC99                               >>
-  put #echo %helpecho <<    Class                                                         >>
-  put #echo %helpecho <<      Which classes should automapper turn on and off?            >>
-  put #echo %helpecho <<      #var automapper.class -arrive -combat -joust -racial -rp    >>
-  put #echo %helpecho <<  Now save! (#save vars for Genie | cmd-s for Outlander)          >>
-  put #echo %helpecho <<                                                                  >>
-  put #echo %helpecho <<  try `.automapper walk` for help with the various walk types     >>
-  put #echo %helpecho <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+  put #echo %helpecho <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+  put #echo %helpecho <<  Welcome to automapper Setup!   (version %autoversion)             >>
+  put #echo %helpecho <<  Use the command line to set the following preferences:            >>
+  put #echo %helpecho <<    Typeahead                                                       >>
+  put #echo %helpecho <<      Standard Account = 1, Premium Account = 2, LTB Purchase = +1  >>
+  put #echo %helpecho <<      #var automapper.typeahead 1                                   >>
+  put #echo %helpecho <<    Pause                                                           >>
+  put #echo %helpecho <<      Time to pause before sending a "put x" command                >>
+  put #echo %helpecho <<      #var automapper.pause 0.01                                    >>
+  put #echo %helpecho <<    Confirmation                                                    >>
+  put #echo %helpecho <<      1: wait for correct confirmation of sent commands             >>
+  put #echo %helpecho <<      0: don't wait                                                 >>
+  put #echo %helpecho <<      #var automapper.confirmation 1                                >>
+  put #echo %helpecho <<    Infinite Loop Protection                                        >>
+  put #echo %helpecho <<      Increase if you get infinte loop errors                       >>
+  put #echo %helpecho <<      #var automapper.loop 0.001                                    >>
+  put #echo %helpecho <<    Echoes                                                          >>
+  put #echo %helpecho <<      how verbose do you want automapper to be?                     >>
+  put #echo %helpecho <<      #var automapper.verbose 1                                     >>
+  put #echo %helpecho <<    Ice Road Behavior                                               >>
+  put #echo %helpecho <<      1: collect rocks on the ice road when lacking skates          >>
+  put #echo %helpecho <<      0: just wait 15 seconds with no RT instead                    >>
+  put #echo %helpecho <<      #var automapper.iceroadcollect 1                              >>
+  put #echo %helpecho <<    Color                                                           >>
+  put #echo %helpecho <<      What should the default automapper echo color be?             >>
+  put #echo %helpecho <<      #var automapper.color #33CC99                                 >>
+  put #echo %helpecho <<    Class                                                           >>
+  put #echo %helpecho <<      Which classes should automapper turn on and off?              >>
+  put #echo %helpecho <<      #var automapper.class -arrive -combat -joust -racial -rp      >>
+  put #echo %helpecho <<  Now save! (#save vars for Genie | cmd-s for Outlander)            >>
+  put #echo %helpecho <<                                                                    >>
+  put #echo %helpecho <<  try `.automapper walk` for help with the various walk types       >>
+  put #echo %helpecho <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
   exit
 }
 if matchre("%1", "^walk$") then {
