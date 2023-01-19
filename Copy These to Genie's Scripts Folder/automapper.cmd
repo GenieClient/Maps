f6088178aa8531573694e9966a07b2998b395834
HanryuDR <HanryuDR@users.noreply.github.com>
2023-01-19 08:25:14 -0500

    comment at top re: automapper help

--- Copy These to Genie's Scripts Folder/automapper.cmd
+++ Copy These to Genie's Scripts Folder/automapper.cmd
@@ -1,5 +1,6 @@
 # automapper.cmd
-var autoversion 8.2023-1-5
+var autoversion 8.2023-01-08
+# use '.automapper help' from the command line for variables and more
 # debug 5 is for outlander; genie debuglevel 10
 #debuglevel 10
 #debug 5
