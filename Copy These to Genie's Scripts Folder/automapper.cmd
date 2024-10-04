3d77f5597e40eda281a28d8ca873c4f6533a93e4
HanryuDR <HanryuDR@users.noreply.github.com>
2024-10-04 08:41:38 -0400

    fixed search rooms in maps i can jump to in DRTest

--- Copy These to Genie's Scripts Folder/automapper.cmd
+++ Copy These to Genie's Scripts Folder/automapper.cmd
@@ -916,8 +916,9 @@ MOVE.SEARCH2:
 #Roundtime: 2 sec.
 #You don't find anything of interest here.
   var action_retry ^You find some interesting signs something is here\.$
-  var success ^You (?:find|notice|push|scan|walk) .+? (?:alcove|crevice|gap|path|spot|tracks|trail|trapdoor)|^After a careful search of the area|^As you search, the air shimmers|^Just under the Bridge of Sparrows|^There seems to be some
+  var success ^You (?:find|notice|push|scan|walk) .+? (?:alcove|crevice|door|gap|log|opening|path|spot|tracks|trail|trapdoor|wall)|^After a careful search of the area|^As you search, the air shimmers|^Just under the Bridge of Sparrows|^There seems to be some
   #4-408 After a careful search of the area, you discover a trampled path!
+  #4a-11 You find a very faint trail through the grass.
   #7-373 You notice a path hidden in the brush that leads under the stone bridge.
   #7-395 There seems to be some sort of path leading to the east.
   #7-737 You scan the cave's shadows and find a small alcove!
@@ -935,13 +936,21 @@ MOVE.SEARCH2:
   #11-103 You find a faint trail.|You find that the faint trail is right where you found it before.
   #11-105 You find a faint trail.
   #11-119 You find a faint trail.|You find that the faint trail is right where you found it before.
+  #30-391 THIEF WILL HAVE TO FIX THIS ONE.
   #40-136 You push through bushes and around shrubbery and discover a faint forest trail!
+  #48-2 I'M NOT BOTHERING TO DO THIS ONE.
   #66-199 You find a dirt path!
   #66-319 As you search, the air shimmers and a dark onyx arch appears!
   #67-29 Just under the Bridge of Sparrows you notice a partially concealed channel bank.
   #69-6 You find a narrow path.
+  #90-798 You find an open wall panel between two support beams.
   #92-92 You find a narrow gap.|You find that the narrow gap is right where you found it before.
   #92-201 You find a trapdoor.
+  #98-33 You find a dark opening in the fissure.
+  #98-167 You find a fallen log.
+  #98-313 You find a hidden door.|You find that the hidden door is right where you found it before.
+  #106-5 I'M NOT BOTHERING TO DO THIS ONE.
+  #108-354 You find some footholds leading up the rock wall.
 
   var action search
   gosub ACTION.MAPPER.ON
@@ -954,8 +963,7 @@ MOVE.OBJSEARCH:
   pause %command_pause
   if ($roundtime > 0) then pause %command_pause
   if ($broom_carpet) then eval movement replacere("%movement", "climb ", "go ")
-  put %movement
-  goto MOVE.DONE
+  goto DO.MOVE
 
 MOVE.SCRIPT:
   var subscript 1
